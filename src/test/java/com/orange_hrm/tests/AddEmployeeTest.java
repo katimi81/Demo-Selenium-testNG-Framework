59a423317a842efbcf62dae28f4aa44484798d57
katimi81 <katimi@gmail.com>
2024-05-09 17:37:19 -0400

    validated login details when checkbox "Create Login Details" is selected

--- src/test/java/com/orange_hrm/tests/AddEmployeeTest.java
+++ src/test/java/com/orange_hrm/tests/AddEmployeeTest.java
@@ -13,22 +13,16 @@ public class AddEmployeeTest extends TestBase {
     @Test
     public void addEmployee() throws InterruptedException {
         LoginPage loginPage = new LoginPage(driver);
-        Thread.sleep(2000);
         loginPage.login("yoll-student", "Bootcamp5#");
-        Thread.sleep(2000);
         HomePage homePage = new HomePage(driver);
         homePage.clickPimTab();
         PimPage pimPage = new PimPage(driver);
         pimPage.clickAddEmployeeTab();
         pimPage.fillEmployeeDetails();
         pimPage.clickCreateLoginDetailsCheckbox();
-//        System.out.println("Text: "+pimPage.getPasswordRecommendationText());
         Assert.assertEquals(pimPage.getPasswordRecommendationText(), ExpectedMessages.PASSWORD_RECOMMENDATION);
-        Thread.sleep(2000);
         Assert.assertTrue(pimPage.verifyLoginDetailsAreDisplayed());
-        Thread.sleep(2000);
         pimPage.clickCreateLoginDetailsCheckbox();
-        Thread.sleep(2000);
         Assert.assertFalse(pimPage.verifyLoginDetailsAreDisplayed());
     }
 
